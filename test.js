c227962adb4cf44984506668f10c14de191c4a9f
Erik Jensson <erik@jensson.no>
2023-09-08 15:14:31 +0200

    oops

--- test.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-const content = fs.readFileSync("./data/Postnummerregister-ansi.txt", "utf8");
-let res = new Map();
-const data = content.split("\n").map((line) => {
-  const [zipcode, city] = line.split("\t");
-  res.set(zipcode, city);
-});
-
-console.log(res);
